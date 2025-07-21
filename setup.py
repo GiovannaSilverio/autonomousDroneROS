1d3ec8df97efe8a8079d302a63e1ce68d25b2945
Giovanna Silverio <giosilverio.10@gmail.com>
2025-07-21 14:33:18 -0300

    correcao arquivo setup.py e package.xml

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
-from setuptools import find_packages, setup
+from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'drone_control'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -18,8 +22,12 @@ setup(
     description='TODO: Package description',
     license='TODO: License declaration',
     tests_require=['pytest'],
+
     entry_points={
         'console_scripts': [
+            'drone_node = drone_control.no_drone:main',
+            'lidar_node = drone_control.no_lidar:main',
+            'comando_node = drone_control.no_comando:main',
         ],
     },
 )
